36bb3cecce84e58d12d87e19bb79db544b007f35
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-05-09 20:57:29 +0530

    modified

--- app/config.py
+++ app/config.py
@@ -1,14 +1,12 @@
 import os
 
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
 
 class Config:
 
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "dev-secret"
+    )
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL"
